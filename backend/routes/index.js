f3efe92427e74302fdc1510e2e70fca6e9194aef
zaidjawed <zaidjaved182@gmail.com>
2022-04-05 19:26:59 +0530

    jwt

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const jwt = require("jsonwebtoken");
 const {
   v1: uuidv1,
   v4: uuidv4,
@@ -7,12 +8,10 @@ const {
 
 const  { connectToDb } = require("../models/database")
 const { hashPassword, verifyPassword } = require("../utils/password");
+const accessTokenSecret = JSON.parse(process.env.ACCESS_TOKEN_SECRET);
 
-const signup = function(req,res,next) {
+const signup = function(req,res) {
   const { name, email, password, role } = req.body;
-
-  console.log("next")
-  console.log(next)
 
   let userDetails = {
     id: uuidv1(),
@@ -60,7 +59,7 @@ const signup = function(req,res,next) {
   })
 }
 
-const login = function(req,res,next) {
+const login = function(req,res) {
   const { email, password } = req.body;
 
   connectToDb()
@@ -79,6 +78,19 @@ const login = function(req,res,next) {
       }
       else {
         if(verifyPassword(password, user[0].password)){
+          //set tokens
+          const payload = {name: user[0].name, email: user[0].email, role: user[0].role};
+          const token = jwt.sign(payload, accessTokenSecret.key, {
+            algorithm: accessTokenSecret.type,
+            expiresIn: `${accessTokenSecret.expiresIn}m`,
+          })
+          const cookieConfig = {
+            httpOnly: true,
+            sameSite: "none",
+            secure: false,
+          };
+          res.cookie("accessToken", token, cookieConfig);
+          
           return res
           .status(200)
           .json({ message: "Correct password", user: {name: user[0].name, email: user[0].email, role: user[0].role}});
@@ -97,6 +109,35 @@ const login = function(req,res,next) {
   })
 }
 
+const authenticate = function(req, res, next) {
+	const token = req.cookies.accessToken;
+  console.log("cooke/dashboard",req.cookies);
+
+	if (!token) {
+		return res
+    .status(402)
+    .json({type: "Internal Server Error", message: "No token"});
+	}
+
+	var payload
+	try {
+		payload = jwt.verify(token, accessTokenSecret.key)
+	} catch (e) {
+		if (e instanceof jwt.JsonWebTokenError) {
+			return res
+      .status(403)
+      .json({type: "Internal Server Error", message: "Token wrong"});
+		}
+		return res
+    .status(500)
+    .json({type: "Internal Server Error", message: "An error has occurred. Please try again later."});
+	}
+  console.log(payload);
+	return next();
+  // .status(200)
+  // .json({message: payload});
+}
+
 router.post(
   "/signup",
   signup
@@ -106,5 +147,14 @@ router.post(
   "/signin",
   login
 );
+
+router.get(
+  "/dashboard",
+  authenticate,
+  (req,res) => {
+    console.log(req)
+    res.status(200).json({message: "dashboard"})
+  }
+)
 
 module.exports = router;
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from '../../axios';
 
 export default function Dashboard() {
+
+  useEffect(() => {
+    axios.get("/dashboard")
+    .then(res => console.log(res))
+    .catch(err => console.log({...err}));
+  })
+
+
   return (
     <div>Dashboard</div>
   )
