adcbebea5ce8e8bd84feb739a70a837e7dc95b24
Sharique Nadim <ramps4u2@gmail.com>
2022-04-12 00:00:58 +0530

    Title + favicon + simple edits

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,16 +1,17 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import axios from '../../axios';
 
 export default function Dashboard() {
 
-  useEffect(() => {
-    axios.get("/dashboard")
-    .then(res => console.log(res))
-    .catch(err => console.log({...err}));
-  })
+    useEffect(() => {
+        axios.get("/dashboard")
+            .then(res => console.log(res))
+            .catch(err => console.log({...err }));
+    })
 
 
-  return (
-    <div>Dashboard</div>
-  )
+    return ( <
+        div > Dashboard
+        for admin < /div>
+    )
 }
--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -1,103 +1,121 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from "react-router-dom";
 import axios from '../../axios';
 import './index.scss';
 
 export default function SignUp() {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassoword, setConfirmPassoword] = useState("");
-  const [name, setName] = useState("");
+    const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassoword, setConfirmPassoword] = useState("");
+    const [name, setName] = useState("");
 
-  const handleSubmit = function(e) {
-    e.preventDefault();
+    const handleSubmit = function(e) {
+        e.preventDefault();
 
-    const userDetails = {
-      name: name,
-      password: password,
-      role: "admin",
-      email: email
+        const userDetails = {
+            name: name,
+            password: password,
+            role: "admin",
+            email: email
+        }
+
+        axios.post("/signup", userDetails)
+            .then((res) => {
+                console.log(res)
+                history.push("/dashboard");
+            })
+            .catch(err => {
+                console.log(err.response.data.message);
+            })
     }
 
-    axios.post("/signup", userDetails)
-    .then((res) => {
-      console.log(res)
-      history.push("/dashboard");
-    })
-    .catch(err => {
-      console.log(err.response.data.message);
-    })
-  }
+    return ( <
+        div className = "signup__container" >
+        <
+        div className = "signup__form--container" >
+        <
+        div className = "signup__form" >
+        <
+        div className = "signup__form--header" >
+        <
+        h1 > Wellity Management System < /h1> <
+        /div>
 
-  return (
-    <div className="signup__container">
-      <div className="signup__form--container">
-        <div className="signup__form">
-          <div className="signup__form--header">
-            <h1>Wellity Management System</h1>
-          </div>
-
-          <div className="signup__form--body">
-            <form action="#" className="signup__form--form" onSubmit={handleSubmit}>
-              <div className="signup__form--item">
-                <div className="signup__form--title">
-                  <h2>Register</h2>
-                </div>
-                <label htmlFor="">Email</label>
-                <input 
-                  type="email" 
-                  required 
-                  value={email} 
-                  onChange={(e) => {
-                    setEmail(e.target.value)
-                  }}
-                />
-              </div>
-              <div className="signup__form--item">
-                <label htmlFor="">Password</label>
-                <input 
-                  type="password" 
-                  required 
-                  value={password} 
-                  onChange={(e) => {
-                    setPassword(e.target.value)
-                  }}
-                />
-              </div>
-              <div className="signup__form--item">
-                <label htmlFor="">Confirm password</label>
-                <input 
-                  type="password" 
-                  required 
-                  value={confirmPassoword} 
-                  onChange={(e) => {
-                    setConfirmPassoword(e.target.value)
-                  }}
-                />
-              </div>
-              <div className="signup__form--item">
-                <label htmlFor="">Name</label>
-                <input 
-                  type="text" 
-                  required 
-                  value={name} 
-                  onChange={(e) => {
-                    setName(e.target.value)
-                  }}
-                />
-              </div>
-              <div className="signup__form--item">
-                <input type="submit" value="Register" />
-              </div>
-              <div className="signup__form--footer">
-                Already have an account ? &nbsp;
-                <Link to="/signin">login</Link>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+        <
+        div className = "signup__form--body" >
+        <
+        form action = "#"
+        className = "signup__form--form"
+        onSubmit = { handleSubmit } >
+        <
+        div className = "signup__form--item" >
+        <
+        div className = "signup__form--title" >
+        <
+        h2 > Register < /h2> <
+        /div> <
+        label htmlFor = "" > Email < /label> <
+        input type = "email"
+        required value = { email }
+        onChange = {
+            (e) => {
+                setEmail(e.target.value)
+            }
+        }
+        /> <
+        /div> <
+        div className = "signup__form--item" >
+        <
+        label htmlFor = "" > Password < /label> <
+        input type = "password"
+        required value = { password }
+        onChange = {
+            (e) => {
+                setPassword(e.target.value)
+            }
+        }
+        /> <
+        /div> <
+        div className = "signup__form--item" >
+        <
+        label htmlFor = "" > Confirm password < /label> <
+        input type = "password"
+        required value = { confirmPassoword }
+        onChange = {
+            (e) => {
+                setConfirmPassoword(e.target.value)
+            }
+        }
+        /> <
+        /div> <
+        div className = "signup__form--item" >
+        <
+        label htmlFor = "" > Name < /label> <
+        input type = "text"
+        required value = { name }
+        onChange = {
+            (e) => {
+                setName(e.target.value)
+            }
+        }
+        /> <
+        /div> <
+        div className = "signup__form--item" >
+        <
+        input type = "submit"
+        value = "Register" / >
+        <
+        /div> {
+            /* <div className="signup__form--footer">
+                            Already have an account ? &nbsp;
+                            <Link to="/signin">login</Link>
+                          </div> */
+        } <
+        /form> <
+        /div> <
+        /div> <
+        /div> <
+        /div>
+    )
 }
